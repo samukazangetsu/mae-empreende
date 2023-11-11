b6f3799e99381c05f30784a59d03334b0a9a7f79
Samuel Alves Coutinho <samuel_alves_coutinho@hotmail.com>
2023-11-11 18:21:05 -0300

    feat: adiciona dependencias do sqlite e express

--- api/routes/app.js
+++ api/routes/app.js
@@ -1,53 +1,58 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('../config/dbConnection.js');
+import { openDb } from '../src/configDB.js';
+
+import express from 'express';
+import bodyParser  from 'body-parser';
+// import { db } from '../config/dbConnection.js'
 
 const app = express();
+app.use(express.json())
 const port = 3000;
 
 // consultas sql de exemplo
-const LISTAR_USUARIO = 'SELECT * FROM usuarios WHERE id = ?';
-const INSERIR_USUARIO = 'INSERT INTO usuarios SET ?';
-const ATUALIZAR_USUARIO = 'UPDATE usuarios SET ? WHERE id = ?';
-const EXCLUIR_USUARIO = 'DELETE FROM usuarios WHERE id = ?';
+// const LISTAR_USUARIO = 'SELECT * FROM usuarios WHERE id = ?';
+// const INSERIR_USUARIO = 'INSERT INTO usuarios SET ?';
+// const ATUALIZAR_USUARIO = 'UPDATE usuarios SET ? WHERE id = ?';
+// const EXCLUIR_USUARIO = 'DELETE FROM usuarios WHERE id = ?';
 
-app.use(bodyParser.json());
+openDb();
+
+// app.use(bodyParser.json());
 
 // listar todos os usuários
-app.get('/users/:id', (req, res) => {
-  db.query(LISTAR_USUARIO, (err, result) => {
-    if (err) throw err;
-    res.json(result);
-  });
-});
+// app.get('/users/:id', (req, res) => {
+//   db.query(LISTAR_USUARIO, (err, result) => {
+//     if (err) throw err;
+//     res.json(result);
+//   });
+// });
 
-// inserir um novo usuário
-app.post('/users', (req, res) => {
-  const { username, email } = req.body;
-  db.query(INSERIR_USUARIO, [username, email], (err, result) => {
-    if (err) throw err;
-    res.json({ message: 'Usuário inserido com sucesso', id: result.insertId });
-  });
-});
+// // inserir um novo usuário
+// app.post('/users', (req, res) => {
+//   const { username, email } = req.body;
+//   db.query(INSERIR_USUARIO, [username, email], (err, result) => {
+//     if (err) throw err;
+//     res.json({ message: 'Usuário inserido com sucesso', id: result.insertId });
+//   });
+// });
 
-// atualizar um usuário
-app.put('/users/:id', (req, res) => {
-  const { username, email } = req.body;
-  const userId = req.params.id;
-  db.query(ATUALIZAR_USUARIO, [username, email, userId], (err, result) => {
-    if (err) throw err;
-    res.json({ message: 'Usuário atualizado com sucesso' });
-  });
-});
+// // atualizar um usuário
+// app.put('/users/:id', (req, res) => {
+//   const { username, email } = req.body;
+//   const userId = req.params.id;
+//   db.query(ATUALIZAR_USUARIO, [username, email, userId], (err, result) => {
+//     if (err) throw err;
+//     res.json({ message: 'Usuário atualizado com sucesso' });
+//   });
+// });
 
-// excluir um usuário
-app.delete('/users/:id', (req, res) => {
-  const userId = req.params.id;
-  db.query(EXCLUIR_USUARIO, userId, (err, result) => {
-    if (err) throw err;
-    res.json({ message: 'Usuário excluído com sucesso' });
-  });
-});
+// // excluir um usuário
+// app.delete('/users/:id', (req, res) => {
+//   const userId = req.params.id;
+//   db.query(EXCLUIR_USUARIO, userId, (err, result) => {
+//     if (err) throw err;
+//     res.json({ message: 'Usuário excluído com sucesso' });
+//   });
+// });
 
 
 app.listen(port, () => {
