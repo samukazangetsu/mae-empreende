c7e20212bc930dc13f65214a458713e91688fbec
Samuel Alves Coutinho <samuel_alves_coutinho@hotmail.com>
2023-11-11 19:17:52 -0300

    feat: cria tabela usuário

--- db/config/configDB.js
+++ db/config/configDB.js
@@ -4,7 +4,7 @@ import { open } from 'sqlite'
 // you would have to import / invoke this in another file
 export async function openDb () {
   return open({
-    filename: '../../database.db',
+    filename: './database.db',
     driver: sqlite3.Database
   }).catch(function () {
     console.log("Falha ao abrir DB");
--- db/controller/usuario.js
+++ db/controller/usuario.js
@@ -2,7 +2,15 @@ import { openDb } from "../config/configDB.js";
 
 export default async function createTable() {
     openDb().then(db => {
-        db.exec('CREATE TABLE IF NOT EXISTS Usuario (id INTEGER PRIMARY KEY)');
+        db.exec(`CREATE TABLE IF NOT EXISTS Usuario (
+            id INTEGER PRIMARY KEY NOT NULL, 
+            nome STRING, 
+            email STRING, 
+            senha STRING,
+            telefone STRING,
+            cpf STRING
+            )`
+        );
         console.log('Tabela criada');
     });
 }
