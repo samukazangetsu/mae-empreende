8b5c5957322ba8fd54a0b990d1b5f4abf03ee9e2
Rodrigo <rodrigogayergomes@gmail.com>
2023-11-16 23:50:12 -0300

    criando rotas api de inserção e atualização

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,34 +1,84 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { openDb } from '../../db/config/configDB.js';
 
 const router = express();
 router.use(express.json());
+router.use(bodyParser.json());
 
 const root = 'web';
 
-router.get("/", function(req, res) {
+const INSERIR_USUARIO = `
+    INSERT INTO Usuarios (nome, email, senha, telefone, cpf, endereco_id)
+    VALUES (?, ?, ?, ?, ?, ?)
+  `;
+
+const ATUALIZAR_USUARIO = `
+      UPDATE Usuarios
+      SET nome = ?, email = ?, senha = ?, telefone = ?, cpf = ?, endereco_id = ?
+      WHERE id = ?
+    `;
+
+const INSERIR_ENDERECO = `
+      INSERT INTO Enderecos (bairro, cep, logradouro, numero, complemento, cidade, estado)
+      VALUES (?, ?, ?, ?, ?, ?, ?)
+    `;
+
+const ATUALIZAR_ENDERECO = `
+      UPDATE Enderecos
+      SET bairro = ?, cep = ?, logradouro = ?, numero = ?, complemento = ?, cidade = ?, estado = ?
+      WHERE endereco_id = ?
+    `;
+
+
+router.get("/", function (req, res) {
     res.redirect('/cadastro');
 });
 
-router.get("/cadastro", function(req, res) {
+router.get("/cadastro", function (req, res) {
     console.log('Rota cadastro acessada')
-    res.sendFile('/cadastro/cadastro.html', {root: root});
+    res.sendFile('/cadastro/cadastro.html', { root: root });
 });
 
-router.get("/sucesso-cadastro", function(req, res) {
-    
-    res.sendFile('/cadastro/sucesso_cadastro.html', {root: root});
+router.get("/sucesso-cadastro", function (req, res) {
+
+    res.sendFile('/cadastro/sucesso_cadastro.html', { root: root });
 });
 
-export default router;
 
-// consultas sql de exemplo
-// const LISTAR_USUARIO = 'SELECT * FROM usuarios WHERE id = ?';
-// const INSERIR_USUARIO = 'INSERT INTO usuarios SET ?';
-// const ATUALIZAR_USUARIO = 'UPDATE usuarios SET ? WHERE id = ?';
-// const EXCLUIR_USUARIO = 'DELETE FROM usuarios WHERE id = ?';
+// ----------------------------------------------------------------
+// Métodos para tabela 'Usuários'
+// ----------------------------------------------------------------
 
+router.post('/usuarios', async (req, res) => {
+    try {
+        const { nome, email, senha, telefone, cpf, endereco_id } = req.body;
+        const db = await openDb();
+        await db.run(INSERIR_USUARIO, [nome, email, senha, telefone, cpf, endereco_id]);
+        console.log(`Usuário inserido com sucesso`);
+        res.status(201).send(`Usuário inserido com sucesso`);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send(`Erro ao inserir usuário: ${error.message}`);
+    }
+});
 
-// app.use(bodyParser.json());
+router.put('/usuarios/:id', async (req, res) => {
+    try {
+        const idUsuario = req.params.id;
+        const { nome, email, senha, telefone, cpf, endereco_id } = req.body;
+        const db = await openDb();
+        await db.run(ATUALIZAR_USUARIO, [nome, email, senha, telefone, cpf, endereco_id, idUsuario]);
+        if (this.changes === 0) {
+            return res.status(404).send('Usuário não encontrado');
+        }
+        console.log(`Infos de usuário ${idUsuario} atualizadas`);
+        res.status(200).send('Informações do usuário atualizadas com sucesso');
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send(`Erro ao atualizar usuário: ${error.message}`);
+    }
+});
 
 // listar todos os usuários
 // app.get('/users/:id', (req, res) => {
@@ -38,26 +88,7 @@ export default router;
 //   });
 // });
 
-// // inserir um novo usuário
-// app.post('/users', (req, res) => {
-//   const { username, email } = req.body;
-//   db.query(INSERIR_USUARIO, [username, email], (err, result) => {
-//     if (err) throw err;
-//     res.json({ message: 'Usuário inserido com sucesso', id: result.insertId });
-//   });
-// });
-
-// // atualizar um usuário
-// app.put('/users/:id', (req, res) => {
-//   const { username, email } = req.body;
-//   const userId = req.params.id;
-//   db.query(ATUALIZAR_USUARIO, [username, email, userId], (err, result) => {
-//     if (err) throw err;
-//     res.json({ message: 'Usuário atualizado com sucesso' });
-//   });
-// });
-
-// // excluir um usuário
+// excluir um usuário
 // app.delete('/users/:id', (req, res) => {
 //   const userId = req.params.id;
 //   db.query(EXCLUIR_USUARIO, userId, (err, result) => {
@@ -66,4 +97,71 @@ export default router;
 //   });
 // });
 
+// ---------------------------------------------------------------------------------------
+// Métodos para tabela 'Endereco'
+// ---------------------------------------------------------------------------------------
 
+router.post('/enderecos', async (req, res) => {
+    try {
+        const { bairro, cep, logradouro, numero, complemento, cidade, estado } = req.body;
+        const db = await openDb();
+        await db.run(INSERIR_ENDERECO, [bairro, cep, logradouro, numero, complemento, cidade, estado]);
+        console.log(`Endereço inserido com sucesso`);
+        res.status(201).send(`Endereço inserido com sucesso`);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send(`Erro ao inserir endereço: ${error.message}`);
+    }
+});
+
+router.put('/enderecos/:id', async (req, res) => {
+    try {
+        const idEndereco = req.params.id;
+        const { bairro, cep, logradouro, numero, complemento, cidade, estado } = req.body;
+        const db = await openDb();
+        await db.run(ATUALIZAR_ENDERECO, [bairro, cep, logradouro, numero, complemento, cidade, estado, idEndereco]);
+        if (this.changes === 0) {
+            return res.status(404).send('Endereço não encontrado');
+        }
+        console.log(`Informações do endereço com o ID ${idEndereco} atualizadas`);
+        res.status(200).send(`Informações do endereço com o ID ${idEndereco} atualizadas`);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send(`Erro ao atualizar endereço: ${error.message}`);
+    }
+});
+
+export default router;
+
+/////////////////////////////////////////////////////////////////////
+// exemplo de função para cadastro (inserção de usuário na tabela) //
+// 
+/////////////////////////////////////////////////////////////////////
+/* <script>
+        function enviarUsuario() {
+            const form = document.getElementById('usuarioForm');
+            const formData = new FormData(form);
+
+            fetch('/usuarios', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(Object.fromEntries(formData)),
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao cadastrar usuário');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                alert('Usuário cadastrado');
+            })
+            .catch(error => {
+                console.error('Erro:', error);
+                alert('Erro ao cadastrar usuário');
+            });
+        }
+</script> */
